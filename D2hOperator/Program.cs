19e36fe950e2cc122c880376179de91ee8e3e90b
gavand-sagar <gavand.sagar.s@gmail.com>
2022-09-27 09:58:27 +0530

    Add project files.

---
+++ D2hOperator/Program.cs
@@ -0,0 +1,31 @@
+﻿using DataAccess.Entities;
+using DataAccess.Operations;
+using System;
+using System.Collections.Generic;
+namespace D2hOperator
+{
+    
+    internal class Program
+    {
+        private static string connectionString = @"Data Source=desktop-5vqtp5k;Initial Catalog=d2hoperator;Integrated Security=True";
+
+        private static CityOperations _cityOperations = new CityOperations(connectionString);
+        static void Main(string[] args)
+        {
+
+            //Console.WriteLine("Enter starting letter");
+
+            //string input = Console.ReadLine();
+
+            //List<City> cityList = _cityOperations.GetCities(input);
+
+            //foreach (City c in cityList)
+            //{
+            //    Console.WriteLine(c.id);
+            //    Console.WriteLine(c.name);
+            //}
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DataAccess/Operations/CityOperations.cs
@@ -0,0 +1,45 @@
+﻿using DataAccess.Entities;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace DataAccess.Operations
+{
+    public class CityOperations
+    {
+        private string _connectionString;
+        public CityOperations(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+        public List<City> GetCities(string input)
+        {
+            List<City> cities = new List<City>();
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+
+                SqlCommand command = new SqlCommand($"select c.[id],c.[name] from city as c where c.[name] like '{input}%'", connection);
+
+                connection.Open();
+
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    City c = new City();
+                    c.id = (int)reader[0];
+                    c.name = (string)reader[1];
+
+                    cities.Add(c);
+
+                }
+
+                connection.Close();
+
+            }
+
+            return cities;
+        }
+    }
+}
